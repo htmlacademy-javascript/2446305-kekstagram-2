17ea153cba771f4e4d242a49b61100213677d7c9
Kseniia Meshkova <dr.meshkovak@gmail.com>
2024-10-29 20:44:44 +0400

    Делает домашнее задание

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const id [];
+for (let i=1; i <=25; i ++) {
+    const obj = {
+        id: i,
+        avatar: 'img/avatar-6.svg',
+        name: Андрей,
+        description: Отель на берегу моря,
+        message: 'В целом всё неплохо. Но не всё.',
+    }
+    id push(obj)
+}
+
+console.log (id)
+
+const likes [];
+for let (i=15; i <=200; i ++) {
+    console.log (getLikes)
+    }
+
+    const id [];
+for let (i=1; i <=25; i ++) {
+    const obj = {
+        id: i,
+        avatar: 'img/avatar-1.svg',
+        name: Кирилл,
+        description: Указатель дороги на пляж,
+        message: 'Всё отлично!',
+    }
+    id push(obj)
+}
+
+console.log (id)
+
