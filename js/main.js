fed431a59d716261f4595a3f3a6b7f373360237b
Kseniia Meshkova <dr.meshkovak@gmail.com>
2024-11-12 15:15:07 +0400

    Делает домашнее задание Кексограм

--- js/main.js
+++ js/main.js
@@ -1,33 +1,76 @@
-const id [];
-for (let i=1; i <=25; i ++) {
-    const obj = {
-        id: i,
-        avatar: 'img/avatar-6.svg',
-        name: Андрей,
-        description: Отель на берегу моря,
-        message: 'В целом всё неплохо. Но не всё.',
+const COUNT_PHOTOS = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const DESCRIPTIONS = ['Пляж', 'Вечер', 'Котик'];
+const COUNT_COMMENTS = 30;
+const MIN_AVATAR = 1;
+const MAX_AVATAR = 6;
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const USERS_NAME = [
+  'Адександр',
+  'Борис',
+  'Олег',
+  'Артем',
+  'Анна',
+  'Юлия'];
+
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));;
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getPhoto = (i) => {
+  return {
+    id: i,
+    url: `photos/ ${i}.jpg`,
+    description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
+    likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+    comments:
+    {
+      id: generatePhotoId(),
+      avatar: `img/avatar-${getRandomInteger(MIN_AVATAR, MAX_AVATAR)}.svg`,
+      message: MESSAGES[getRandomInteger(0, MESSAGES.length - 1)],
+      name: USERS_NAME[getRandomInteger(0, USERS_NAME.length - 1)],
     }
-    id push(obj)
+  }
 }
 
-console.log (id)
 
-const likes [];
-for let (i=15; i <=200; i ++) {
-    console.log (getLikes)
-    }
-
-    const id [];
-for let (i=1; i <=25; i ++) {
-    const obj = {
-        id: i,
-        avatar: 'img/avatar-1.svg',
-        name: Кирилл,
-        description: Указатель дороги на пляж,
-        message: 'Всё отлично!',
-    }
-    id push(obj)
+const getData = () => {
+  const data = [];
+  for (let i = 1; i <= COUNT_PHOTOS; i++) {
+    data.pushgetPhoto(i);
+  }
+  return data;
 }
 
-console.log (id)
+  const getComments = () => {
+  const comments = [];
+  const countComments = getRandomInteger(0, COUNT_COMMENTS);
+  for (let i = 0; i <= countComments; i++) {
+    comments.push({
+      id: generatePhotoId(),
+      avatar: `img/avatar-${getRandomInteger(MIN_AVATAR, MAX_AVATAR)}.svg`,
+      message: MESSAGES[getRandomInteger(0, MESSAGES.length - 1)],
+      name: USERS_NAME[getRandomInteger(0, USERS_NAME.length - 1)],
+    })
+  }
+}
 
+const getDescriptionPhoto = () => {
+  const descriptionPhoto = [];
+  for (let i = 1; i <= COUNT_PHOTOS; i++) {
+    descriptionPhoto.push(getPhoto(i))
+  }
+  return descriptionPhoto;
+}
